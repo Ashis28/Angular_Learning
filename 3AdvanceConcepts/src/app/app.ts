c9b9cd7df3c13970ed55b579338266015d2dece7
Ashis28 <dashashiskumar28@example.com>
2026-05-01 06:09:47 +0530

    project 3 started already

---
+++ 3AdvanceConcepts/src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { ServerStatus } from './dashboard/server-status/server-status';
+import { Traffic } from './dashboard/traffic/traffic';
+import { Tickets } from './dashboard/tickets/tickets';
+import { DashboardItem } from './dashboard/dashboard-item/dashboard-item';
+
+@Component({
+  selector: 'app-root',
+  imports: [Header,ServerStatus,Traffic,Tickets,DashboardItem],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('3AdvanceConcepts');
+  
+  
+}
